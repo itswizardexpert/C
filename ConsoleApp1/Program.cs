f4d858af7138a4133836079ded9bc96b89c66a84
mchugh_kevin <mchugh_kevin@G-02-15>
2022-08-29 10:02:48 -0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Lokes Bot");
+
+            Wizard();
+
+
+            Console.ReadKey();
+        }
+
+        static void Wizard()
+        {
+            double age = 99.9;
+            Console.WriteLine(age);
+
+            Console.WriteLine("Please enter your name:");
+            string name= Console.ReadLine();
+            Console.WriteLine("Hello "+name+" Have a great day");
+        }
+
+    }
+}
